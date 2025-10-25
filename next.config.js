d36613f4b74cadd4475a00b02a641ef9f47971e5
Lykamopia <www.alex94lykam@gmail.com>
2025-10-25 08:59:52 +0000

    C:\Users\Admin\Desktop\nib-procurement>npm run build
    > nextn@0.1.0 buil

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  optimizeFonts: false,
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizeFonts: false,
   },
   images: {
     remotePatterns: [
