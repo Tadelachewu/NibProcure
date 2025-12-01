a170c3775252a9ef66ec45ab22449062973b386f
Lykamopia <www.alex94lykam@gmail.com>
2025-12-01 08:20:17 +0000

    "Remove or disable all 'X-Powered-By' HTTP headers in my web application

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  optimizeFonts: false,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -47,26 +46,6 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
           },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin'
-          }
         ],
       },
     ];
