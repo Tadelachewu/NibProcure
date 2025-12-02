31617109ab0911ae1a689f86a4b3d047a145c4cd
Lykamopia <www.alex94lykam@gmail.com>
2025-12-02 06:42:01 +0000

    Inspect HTTP headers sent by the app. Identify missing or insecure heade

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   poweredByHeader: false,
   typescript: {
@@ -27,7 +28,7 @@ const nextConfig = {
   async headers() {
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      script-src 'self';
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
       img-src 'self' https://placehold.co https://picsum.photos data:;
       font-src 'self' https://fonts.gstatic.com;
@@ -36,17 +37,39 @@ const nextConfig = {
       form-action 'self';
       frame-ancestors 'none';
       upgrade-insecure-requests;
-    `;
+    `.replace(/\s{2,}/g, ' ').trim();
+
+    const securityHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value: cspHeader,
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+      },
+      {
+        key: 'Permissions-Policy',
+        value: "camera=(), microphone=(), geolocation=()",
+      }
+    ];
 
     return [
       {
         source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
+        headers: securityHeaders,
       },
     ];
   },
